9782d825e94ae7f0325384da84dd72bd1ef4f84a
Raj Joshi <rajkiranjoshi@gmail.com>
2021-07-05 22:45:38 +0800

    process_rx_trace: per flow throughput dump to file

--- data_analysis/data-processing/process_rx_trace.py
+++ data_analysis/data-processing/process_rx_trace.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 import csv
 import sys
-
+import os
 
 WINDOW_TIME_LENGTH = 0.001
 DST_IP = "10.1.1.2"
 
 class PerFlowThroughput:
-    def __init__(self, src_port, cc_name):
+    def __init__(self, src_port, cc_name, output_dir):
         self.src_port = src_port
         self.cc_name = cc_name
         self.moving_window = []  # (time, pkt_size)
         self.rate_timeseries = [] # (time, avg rate)
         self.window_sum = 0
         self.start_time = 0
+        filename = output_dir + "/flow_th_{}.dat".format(src_port)
+        self.outfile = open(filename, "w")
+    
+    def __del__(self):
+        self.outfile.close()
+
 
     def add_packet(self, time, pkt_size):
         self.moving_window.append((time, pkt_size))
@@ -25,7 +31,8 @@ class PerFlowThroughput:
             # calculate rate for this window
             rate = self.window_sum / curr_window_len
             rate = (rate * 8 / 1000000) # convert to Mbps
-            self.rate_timeseries.append((end_time, rate))
+            self.outfile.write("{} {:.6f}\n".format(end_time, round(rate, 6)))
+            # self.rate_timeseries.append((end_time, round(rate, 6)))
 
             # move the window forward
             removed_pkt = self.moving_window.pop(0)
@@ -46,10 +53,11 @@ def main():
         print("Usage: {} <csv file>".format(sys.argv[0]))
         sys.exit(1)
 
-    csv_path = sys.argv[1]
+    csv_path = os.path.abspath(sys.argv[1])
+    outdir = os.path.dirname(csv_path)
 
     print("Processing csv file:", csv_path)
-
+    
     csv_file = open(csv_path, 'r')
     csv_reader = csv.reader(csv_file)
     header = next(csv_reader)
@@ -72,7 +80,7 @@ def main():
 
             if src_port not in flows:
                 flows.add(src_port)
-                curr_flow_throughput = PerFlowThroughput(src_port, 'cubic')
+                curr_flow_throughput = PerFlowThroughput(src_port, 'cubic', outdir)
                 flow_throughputs[src_port] = curr_flow_throughput
             else:
                 curr_flow_throughput = flow_throughputs[src_port]
@@ -96,13 +104,13 @@ def main():
     
     print("Processed {} packets".format(pkt_count))
     print("Flow set is:", flows)
-    for flow in flows:
+"""     for flow in flows:
         flow_throughput = flow_throughputs[flow]
         flow_th_file = open("flow_th_{}.dat".format(flow), "w")
         print("Flow: {}, Avg Throughput: {}".format(flow, flow_throughput.get_overall_average()))
         for data_point in flow_throughput.rate_timeseries:
             flow_th_file.write("{} {}\n".format(data_point[0], data_point[1]))
-        flow_th_file.close()
+        flow_th_file.close() """
 
 
 
