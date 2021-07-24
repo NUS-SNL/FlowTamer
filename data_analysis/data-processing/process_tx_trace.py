304c8a4a713237918a461402dd49c2c4298bc9d7
Raj Joshi <rajkiranjoshi@gmail.com>
2021-07-24 17:54:53 +0800

    tx processing: rwnd tracking from tcp ACKs

--- data_analysis/data-processing/process_tx_trace.py
+++ data_analysis/data-processing/process_tx_trace.py
@@ -46,7 +46,33 @@ class BytesInFlightTracker:
         # self.outfile.write("{} {}\n".format(time, bif))
         self.add_to_output_file(time, bif)
     
-
+class RwndTracker:
+    def __init__(self, src_port, ws, output_dir, time, init_rwnd):
+        self.src_port = src_port
+        self.ws = ws
+        self.prev_rwnd = init_rwnd
+        self.prev_rwnd_time = time
+        filename = output_dir + "/flow_rwnd_{}.dat".format(src_port)
+        self.outfile = open(filename, "w")
+        self.outfile.write("{} {}\n".format(self.prev_rwnd_time, self.prev_rwnd))
+        
+    def __del__(self):
+        # print out the latest seen rwnd (ending)
+        self.outfile.write("{} {}\n".format(self.prev_rwnd_time, self.prev_rwnd))
+        self.outfile.close()
+        
+    def track(self, time, rwnd):
+        new_rwnd = rwnd * pow(2, self.ws)
+        if(self.prev_rwnd == new_rwnd): # no change. Update time
+            self.prev_rwnd_time = time
+        else: # rwnd has changed
+            # write the latest timestamped prev_rwnd
+            self.outfile.write("{} {}\n".format(self.prev_rwnd_time, self.prev_rwnd))
+            # update prev_rwnd
+            self.prev_rwnd = new_rwnd
+            self.prev_rwnd_time = time
+            # write it out as well
+            self.outfile.write("{} {}\n".format(self.prev_rwnd_time, self.prev_rwnd))
 
 def main():
     if len(sys.argv) != 2:
@@ -63,12 +89,13 @@ def main():
     header = next(csv_reader)
 
     bif_tracker = None
+    rwnd_tracker = None
 
     pkt_count = 0
     for row in csv_reader:
         pkt_count += 1
         row_len = len(row)
-        if(row_len >= 10): # valid TCP pkt
+        if(row_len >= 12): # valid TCP pkt
             time = float(row[1])
             pktlen = int(row[3])
             src_ip = row[4]
@@ -77,18 +104,25 @@ def main():
             ack_no = int(row[7])
             src_port = int(row[8])
             dst_port = int(row[9])
+            rwnd   = int(row[10])
+            ws   = int(row[11])
             
             if dst_ip != RECEIVER_IP and src_ip != RECEIVER_IP: # random pkt
                 continue
 
             # Not a random pkt
-            if dst_ip == RECEIVER_IP: # data pkt
+            if dst_ip == RECEIVER_IP: # sender --> receiver pkt
                 if bif_tracker == None: # expect 1st valid pkt to be SYN
                     bif_tracker = BytesInFlightTracker(src_port, seq_no, outdir)  
                 bif_tracker.update_tx_bytes(time, pktlen, seq_no)
             
-            elif src_ip == RECEIVER_IP:  # ack pkt
+            elif src_ip == RECEIVER_IP:  # receiver --> sender pkt
+                if rwnd_tracker == None and ws != -1:  # expect 1st valied pkt to be SYN-ACK
+                    rwnd_tracker = RwndTracker(dst_port, ws, outdir, time, rwnd)
+                else:
+                    rwnd_tracker.track(time, rwnd)
                 bif_tracker.update_acked_bytes(time, ack_no)
+                
 
 if __name__ == "__main__":
     main()
