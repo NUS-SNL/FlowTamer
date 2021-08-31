583d14840a1ca00f62f4f2836c42095185294e39
Raj Joshi <rajkiranjoshi@gmail.com>
2021-08-31 08:38:49 +0800

    process_single_flow: bugFix after files re-org

--- data_analysis/trace-analysis/process_single_flow.sh
+++ data_analysis/trace-analysis/process_single_flow.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-pcapParser='/home/raj/Dropbox/NUS-PhD/tofino-p4/harsh-tofino/inNetworkCC/data_analysis/pcap-parser/pcap-parser'
-processTxTrace='/home/raj/Dropbox/NUS-PhD/tofino-p4/harsh-tofino/inNetworkCC/data_analysis/data-processing/process_tx_trace.py'
-processRxTrace='/home/raj/Dropbox/NUS-PhD/tofino-p4/harsh-tofino/inNetworkCC/data_analysis/data-processing/process_rx_trace.py'
+pcapParser='/home/raj/Dropbox/NUS-PhD/tofino-p4/harsh-tofino/inNetworkCC/data_analysis/trace-analysis/pcap-parser'
+processTxTrace='/home/raj/Dropbox/NUS-PhD/tofino-p4/harsh-tofino/inNetworkCC/data_analysis/trace-analysis/process_tx_trace.py'
+processRxTrace='/home/raj/Dropbox/NUS-PhD/tofino-p4/harsh-tofino/inNetworkCC/data_analysis/trace-analysis/process_rx_trace.py'
 
 ${pcapParser} ./*sender*.pcap
 ${pcapParser} ./*receiver*.pcap
